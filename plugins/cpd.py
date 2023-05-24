9a624033ef1dda16aeddec9f776631f7dcb0459f
niranjantechofficial <61946705+niranjantechofficial@users.noreply.github.com>
2023-05-24 13:48:19 +0530

    Update cpd.py

--- plugins/cpd.py
+++ plugins/cpd.py
@@ -135,7 +135,7 @@ async def account_login(bot: Client, m: Message):
                 response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
                 # print(response.json())
                 a = response.json()['url']
-                # print(a)
+                print(a)
                 print("❤❤❤")
 
 
