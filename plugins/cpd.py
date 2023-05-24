27c979447e707b6f6497fe6bec45aa0017fa00a1
niranjantechofficial <61946705+niranjantechofficial@users.noreply.github.com>
2023-05-24 13:23:36 +0530

    Update cpd.py

--- plugins/cpd.py
+++ plugins/cpd.py
@@ -200,6 +200,6 @@ async def account_login(bot: Client, m: Message):
                 await m.reply_text(f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}` & `{url1}`")
                 continue 
     except Exception as e:
-    await m.reply_text(str(e))
-    
+           await m.reply_text(str(e))
+
     await m.reply_text("Done")          
